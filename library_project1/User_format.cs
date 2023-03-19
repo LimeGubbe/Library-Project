9ff7da46841567599d094003bc2a78e9860fa69c
Emil L <91726236+LimeGubbe@users.noreply.github.com>
2023-03-19 15:11:19 +0100

    Add files via upload

---
+++ library_project1/User_format.cs
@@ -0,0 +1,14 @@
+﻿namespace AdminHandlesBooks
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public User(string Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
